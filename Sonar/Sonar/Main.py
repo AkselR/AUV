5e765c75d1790dfd473c5b19d5ce351ab64524ef
Stian Sandve <stian@sandve.org>
2015-01-12 14:45:29 +0100

    Refactoring

--- Sonar/Sonar/Main.py
+++
@@ -1,8 +0,0 @@
-'''
-Created on Jan 12, 2015
-
-@author: stian
-'''
-
-if __name__ == '__main__':
-    pass
---
+++ src/sonar/Main.py
@@ -0,0 +1,22 @@
+'''
+Created on Jan 12, 2015
+
+@author: Stian Sandve
+'''
+
+import Sonar
+import time
+
+def main():
+    sonar = Sonar()
+    
+    try:
+        while True:
+            sonar.send_version()
+            sonar.version_data()
+            time.sleep(0.2)
+    except KeyboardInterrupt:
+        sonar.close()
+
+if __name__ == '__main__':
+    pass
